4e0da0a5c8b076f90019143f09de205a7dd5fa4c
macargen <947736903@qq.com>
2024-07-21 13:49:33 +0800

    change login and database

--- app/src/main/java/com/example/expressage/MainActivity.java
+++ app/src/main/java/com/example/expressage/MainActivity.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.File;
+import java.nio.channels.SelectableChannel;
 
 
 public class MainActivity extends AppCompatActivity{
+
+    SQLiteHelper sqLiteHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +28,26 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        sqLiteHelper=new SQLiteHelper(this);
+
+
         CheckBox btn_company=findViewById(R.id.btn_company);
         CheckBox btn_courier=findViewById(R.id.btn_courier);
         CheckBox btn_customer=findViewById(R.id.btn_customer);
 
         Button btn_login=findViewById(R.id.btn_login);
+        EditText account=findViewById(R.id.editTextTextPassword);
+        EditText password=findViewById(R.id.editTextTextPassword2);
+
+
+
 
         btn_customer.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
+
                 btn_courier.setChecked(false);
                 btn_company.setChecked(false);
             }
@@ -56,24 +72,41 @@ public class MainActivity extends AppCompatActivity{
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //company
-                if(btn_company.isChecked()) {
-                    Intent intent = new Intent();
-                    intent.setClass(MainActivity.this,company.class);
-                    startActivity(intent);
+                String Gnum=account.getText().toString();
+                String Gpasswoed=password.getText().toString();
+//
+//                boolean flag=userDao.checkUser(acc,pas);
+//                if(flag) {
+                SQLiteDatabase sdb=sqLiteHelper.getReadableDatabase();
+                String sql="select * from user where Gnum=? and Gpassword=?";
+                Cursor cursor=sdb.rawQuery(sql, new String[]{Gnum,Gpasswoed});
+                if(cursor.moveToFirst()==true){
+                    cursor.close();
+                    //company
+                    if (btn_company.isChecked()) {
+                        Intent intent = new Intent();
+                        intent.setClass(MainActivity.this, company.class);
+                        startActivity(intent);
+                    }
+                    //courier
+                    if (btn_courier.isChecked()) {
+                        Intent intent = new Intent();
+                        intent.setClass(MainActivity.this, courier.class);
+                        startActivity(intent);
+                    }
+                    //customer
+                    if (btn_customer.isChecked()) {
+                        Intent intent = new Intent();
+                        intent.setClass(MainActivity.this, customer.class);
+                        startActivity(intent);
+                    }
+                }else {
+                    Toast.makeText(MainActivity.this, "账号或密码错误", Toast.LENGTH_SHORT).show();
                 }
-                //courier
-                if(btn_courier.isChecked()){
-                    Intent intent = new Intent();
-                    intent.setClass(MainActivity.this, courier.class);
-                    startActivity(intent);
-                }
-                //customer
-                if(btn_customer.isChecked()){
-                    Intent intent = new Intent();
-                    intent.setClass(MainActivity.this, customer.class);
-                    startActivity(intent);
-                }
+
+//                }else{
+//                    Toast.makeText(MainActivity.this, acc+pas, Toast.LENGTH_SHORT).show();
+//                }
                 if(!btn_company.isChecked()&&!btn_customer.isChecked()&&!btn_courier.isChecked()){
                     Toast.makeText(MainActivity.this, "请选择一个登入身份", Toast.LENGTH_SHORT).show();
                 }
