a84d5b478228ab280b9a06860842a5a29889362d
charles <charlesl2003@163.com>
2024-07-24 21:56:30 +0800

    company can add orders right now by Charles

--- app/src/main/java/com/example/expressage/SQLiteHelper.java
+++ app/src/main/java/com/example/expressage/SQLiteHelper.java
@@ -10,7 +10,7 @@ public class SQLiteHelper extends SQLiteOpenHelper {
 
     private static final String CREATE_USER_TABLE="create table user(num,identity,password,name,gender,phone,address,primary key(num,identity))";
     private static final String CREATE_SEND_TABLE="create table send(NDNum integer primary key autoincrement,num,identity,Sname,Sphone,Sadress,Hname,Cname,foreign key(num,identity) REFERENCES user(num,identity))";
-    private static final String CREATE_GET_TABLE="create table finish(Fnum,num,identity,Dnum,Hname,primary key(Fnum),foreign key(num,identity) REFERENCES user(num, identity))";
+    private static final String CREATE_GET_TABLE="create table finish(Fnum integer primary key autoincrement,num,identity,com_num,com_identity,Hname,foreign key(num,identity) REFERENCES user(num, identity),foreign key(com_num,com_identity) REFERENCES user(num, identity))";
 
     //    public SQLiteHelper( Context context,  String name,  SQLiteDatabase.CursorFactory factory, int version) {
 //        super(context, name, factory, version);
