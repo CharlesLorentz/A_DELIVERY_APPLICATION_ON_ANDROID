76cb8d2d6f7aaf98f0c9d3383ac17f6e7f5eeb99
charles <charlesl2003@163.com>
2024-07-24 22:48:29 +0800

    courier can check finish orders right now by Charles

--- app/src/main/java/com/example/expressage/company.java
+++ app/src/main/java/com/example/expressage/company.java
@@ -1,21 +1,33 @@
 package com.example.expressage;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class company extends AppCompatActivity {
 
-    String num;
+
+    SQLiteHelper sqLiteHelper;
+
+    courier_finish_Dao courier_finish_dao;
+    String customer_num;
+    String goods;
+
+    String my_num;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.company);
-
+        sqLiteHelper=new SQLiteHelper(this);
 
         ImageButton back = findViewById(R.id.btn_back);
         back.setOnClickListener(new View.OnClickListener() {
@@ -26,30 +38,71 @@ public class company extends AppCompatActivity {
         });
 
         Intent user_num = getIntent();
-        num = user_num.getStringExtra("num");
+        my_num = user_num.getStringExtra("num");
         ImageButton my_info=findViewById(R.id.btn_self_info);
         my_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent();
                 intent.setClass(company.this, self_info.class);
-                intent.putExtra("num",num);
+                intent.putExtra("num",my_num);
                 intent.putExtra("identity","company");
                 startActivity(intent);
             }
         });
 
-        Button btn_add=findViewById(R.id.btn_add);
-        btn_add.setOnClickListener(new View.OnClickListener() {
+        courier_finish_dao = new courier_finish_Dao(this);
+
+        EditText get_num = findViewById(R.id.get_num);
+        EditText goods_edit = findViewById(R.id.goods);
+
+        get_num.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent=new Intent();
-                intent.setClass(company.this, company_add.class);
-                intent.putExtra("num",num);
-                intent.putExtra("identity","company");
-                startActivity(intent);
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    get_num.setText("");
+                }
             }
         });
+
+        goods_edit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    goods_edit.setText("");
+                }
+            }
+        });
+
+        Button btn_save=findViewById(R.id.save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("Range")
+            @Override
+            public void onClick(View v) {
+                customer_num=get_num.getText().toString();
+                goods=goods_edit.getText().toString();
+
+                SQLiteDatabase sdb=sqLiteHelper.getReadableDatabase();
+                String sql="select * from user where num=? and identity=?";
+                Cursor cursor=sdb.rawQuery(sql, new String[]{customer_num,"customer"});
+                if(cursor.moveToFirst()==true){}
+                else {
+                    Toast.makeText(company.this, "用户不在本系统中", Toast.LENGTH_SHORT).show();
+                }
+
+
+                boolean result= courier_finish_dao.insertUser(customer_num,"customer",my_num,"company",goods);
+                if(result){
+                    Toast.makeText(company.this, "完成:>", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else{
+                    Toast.makeText(company.this, "失败...", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 }
 
--- app/src/main/java/com/example/expressage/courier.java
+++ app/src/main/java/com/example/expressage/courier.java
@@ -33,7 +33,7 @@ public class courier extends AppCompatActivity {
             }
         });
 
-        Button self_info=findViewById(R.id.self_info);
+        ImageButton self_info=findViewById(R.id.self_info);
         self_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,27 +45,68 @@ public class courier extends AppCompatActivity {
             }
         });
 
-        TextView parcel_info = findViewById(R.id.parcel_info);
-        StringBuilder sb = new StringBuilder();
+        TextView deliver_info = findViewById(R.id.deliver_info);
+        StringBuilder del_sb = new StringBuilder();
 
         SQLiteDatabase kdb =sqLiteHelper.getReadableDatabase();
-        String sql = "select * from send ";
-        Cursor cursor = kdb.rawQuery(sql,null);
+        String del_sql = "select * from send ";
+        Cursor cursor = kdb.rawQuery(del_sql,null);
         if(cursor != null && cursor.moveToFirst()){
             cursor.getColumnIndex("NDNum");
-//            String Num = cursor.getString(cursor.getColumnIndex("Num"));
+            String num = cursor.getString(cursor.getColumnIndex("num"));
             String Get_name = cursor.getString(cursor.getColumnIndex("Sname"));
             String Get_phone = cursor.getString(cursor.getColumnIndex("Sphone"));
             String Sadress = cursor.getString(cursor.getColumnIndex("Sadress"));
             String Hname = cursor.getString(cursor.getColumnIndex("Hname"));
             String Cname = cursor.getString(cursor.getColumnIndex("Cname"));
-            sb.append("收件人姓名: ").append(Get_name).append("\n")
+            del_sb.append("收件人姓名: ").append(Get_name).append("\n")
                     .append("收件人电话: ").append(Get_phone).append("\n")
                     .append("收件人地址: ").append(Sadress).append("\n")
                     .append("货物名称: ").append(Hname).append("\n")
                     .append("物流公司").append(Cname).append("\n") ;
 
-            parcel_info.setText(sb.toString());
+            deliver_info.setText(del_sb.toString());
+        }
+
+        TextView send_info = findViewById(R.id.send_info);
+        StringBuilder send_sb = new StringBuilder();
+
+        String send_sql = "select * from finish";
+        cursor = kdb.rawQuery(send_sql,null);
+        if(cursor != null && cursor.moveToFirst()){
+            String num = cursor.getString(cursor.getColumnIndex("num"));
+            String com_num = cursor.getString(cursor.getColumnIndex("com_num"));
+            String cus_name = "";
+            String cus_phone = "";
+            String cus_address = "";
+            String com_name = "";
+            String com_phone = "";
+            String com_address = "";
+
+            String search_cus_sql = "select * from user where num=? and identity=?";
+            cursor = kdb.rawQuery(search_cus_sql,new String[]{num,"customer"});
+            if(cursor.moveToFirst()==true){
+                cus_name = cursor.getString(cursor.getColumnIndex("name"));
+                cus_phone = cursor.getString(cursor.getColumnIndex("phone"));
+                cus_address = cursor.getString(cursor.getColumnIndex("address"));
+            }
+
+            String search_com_sql = "select * from user where num=? and identity=?";
+            cursor = kdb.rawQuery(search_com_sql,new String[]{com_num,"company"});
+            if(cursor.moveToFirst()==true){
+                com_name = cursor.getString(cursor.getColumnIndex("name"));
+                com_phone = cursor.getString(cursor.getColumnIndex("phone"));
+                com_address = cursor.getString(cursor.getColumnIndex("address"));
+            }
+
+            send_sb.append("收件人姓名: ").append(cus_name).append("\n")
+                    .append("收件人电话: ").append(cus_phone).append("\n")
+                    .append("收件人地址: ").append(cus_address).append("\n")
+                    .append("物流公司名: ").append(com_name).append("\n")
+                    .append("物流公司联系方式: ").append(com_phone).append("\n")
+                    .append("物流公司地址: ").append(com_address).append("\n");
+
+            send_info.setText(send_sb.toString());
         }
     }
 }
